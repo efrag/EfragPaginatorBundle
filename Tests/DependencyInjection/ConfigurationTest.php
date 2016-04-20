76782520ca38a8af839add1212587c1dd07e2076
Eleni Fragkiadaki <eleni.fra@gmail.com>
2016-04-20 22:59:45 +0100

    Adding a test for the configuration of the bundle

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Efrag\Bundle\PaginatorBundle\Tests\DependencyInjection;
+
+use Efrag\Bundle\PaginatorBundle\DependencyInjection\Configuration;
+use Matthias\SymfonyConfigTest\PhpUnit\ConfigurationTestCaseTrait;
+
+/**
+ * Class ConfigurationTest
+ * @package Efrag\Bundle\PaginatorBundle\Tests\DependencyInjection
+ */
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    use ConfigurationTestCaseTrait;
+
+    protected function getConfiguration()
+    {
+        return new Configuration();
+    }
+
+    public function validConfigurations()
+    {
+        $configs = [];
+
+        $configs[] = array(array());
+        $configs[] = array(array('perPage' => 20));
+
+        return $configs;
+    }
+
+    /**
+     * @dataProvider validConfigurations
+     * @param $config
+     */
+    public function testValidConfigurations($config)
+    {
+        $this->assertConfigurationIsValid(array($config));
+    }
+
+    public function invalidConfigurations()
+    {
+        $configs = [];
+
+        $configs[] = array(array('per' => 20));
+        $configs[] = array(array('perPage' => '20'));
+        $configs[] = array(array('perPage' => 'config'));
+
+        return $configs;
+    }
+
+    /**
+     * @dataProvider invalidConfigurations
+     */
+    public function testInvalidConfigurations($config)
+    {
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+}
